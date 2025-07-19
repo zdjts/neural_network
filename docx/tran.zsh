bbbe6ebe83a7aa0b827c5033ffbdc5c4dcb4e092
zdjts <z15188247@gmail.com>
2025-07-19 21:06:26 +0800

    feat: 数据结构最终版本。

--- docx/tran.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-for file in *.doc; do libreoffice --headless --convert-to docx "$file"; done
-
-for file in *.docx; do pandoc "$file" -o "${file%.docx}.tex"; done
--- window/main_window.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "window.h"
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    MainWindow w;
-    w.show();
-    return app.exec();
-}
